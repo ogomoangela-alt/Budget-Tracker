9f112920b51832184d2f36ce8d137d59794cf4a0
Angela Ogomo <ogomoangela@gmail.com>
2026-04-22 12:36:43 +0300

    dark mode added

--- script.js
+++ script.js
@@ -36,6 +36,25 @@ function saveAndRender() {
   localStorage.setItem("transactions", JSON.stringify(transactions));
   render();
 }
+function toggleDarkMode() {
+  document.body.classList.toggle("dark");
+
+  // save preference
+  if (document.body.classList.contains("dark")) {
+    localStorage.setItem("theme", "dark");
+  } else {
+    localStorage.setItem("theme", "light");
+  }
+}
+
+// load theme on startup
+(function () {
+  const savedTheme = localStorage.getItem("theme");
+
+  if (savedTheme === "dark") {
+    document.body.classList.add("dark");
+  }
+})();
 
 function render() {
   const list = document.getElementById("list");
